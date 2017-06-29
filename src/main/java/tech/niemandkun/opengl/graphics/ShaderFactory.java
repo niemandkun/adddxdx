c3c26c46320f8a27858856cbc7fb6af4bebba575
niemandkun <niemandkun@yandex.ru>
2017-06-29 09:32:56 +0500

    simple shader -> default shader

--- src/main/java/tech/niemandkun/opengl/graphics/ShaderFactory.java
+++ src/main/java/tech/niemandkun/opengl/graphics/ShaderFactory.java
@@ -5,19 +5,23 @@ import tech.niemandkun.opengl.engine.Destroyable;
 import java.io.File;
 
 public class ShaderFactory implements Destroyable {
+    private final static String DEFAULT_SHADER_NAME = "default";
+    private final static String FRAGMENT_SHADER_EXT = ".frag";
+    private final static String VERTEX_SHADER_EXT = ".vert";
+
     private final Shader mDefaultShader;
     private final Material mDefaultMaterial;
 
     public ShaderFactory() {
-        mDefaultShader = buildShader("simple");
+        mDefaultShader = buildShader(DEFAULT_SHADER_NAME);
         mDefaultMaterial = new Material(mDefaultShader);
     }
 
     Shader buildShader(String shaderName) {
         try {
             return Shader.getCompiler()
-                    .setFragmentShader(open(shaderName + ".frag"))
-                    .setVertexShader(open(shaderName + ".vert"))
+                    .setFragmentShader(open(shaderName + FRAGMENT_SHADER_EXT))
+                    .setVertexShader(open(shaderName + VERTEX_SHADER_EXT))
                     .compile();
         } catch (ShaderCompileException e) {
             System.out.println(e.getMessage());
